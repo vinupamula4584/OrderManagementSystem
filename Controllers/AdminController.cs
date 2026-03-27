2d670ef4c2fed64b14be7a5b1e0df1c2ef06845e
Narendrababu2424 <Mkamma0878@conestogac.on.ca>
2026-03-27 14:22:23 -0400

    Completed admin dashboard and role management module

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderManagementSystem.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+
+        public IActionResult Roles()
+        {
+            var roles = _roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateRole(string roleName)
+        {
+            if (!string.IsNullOrWhiteSpace(roleName))
+            {
+                if (!await _roleManager.RoleExistsAsync(roleName))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            return RedirectToAction(nameof(Roles));
+        }
+
+        public IActionResult AssignRole()
+        {
+            ViewBag.Users = _userManager.Users.ToList();
+            ViewBag.Roles = _roleManager.Roles.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AssignRole(string userEmail, string roleName)
+        {
+            var user = await _userManager.FindByEmailAsync(userEmail);
+
+            if (user != null && !string.IsNullOrWhiteSpace(roleName))
+            {
+                if (!await _userManager.IsInRoleAsync(user, roleName))
+                {
+                    await _userManager.AddToRoleAsync(user, roleName);
+                }
+            }
+
+            return RedirectToAction(nameof(Users));
+        }
+
+        public IActionResult RemoveRole()
+        {
+            ViewBag.Users = _userManager.Users.ToList();
+            ViewBag.Roles = _roleManager.Roles.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveRole(string userEmail, string roleName)
+        {
+            var user = await _userManager.FindByEmailAsync(userEmail);
+
+            if (user != null && !string.IsNullOrWhiteSpace(roleName))
+            {
+                if (await _userManager.IsInRoleAsync(user, roleName))
+                {
+                    await _userManager.RemoveFromRoleAsync(user, roleName);
+                }
+            }
+
+            return RedirectToAction(nameof(Users));
+        }
+    }
+}
